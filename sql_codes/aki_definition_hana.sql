8794b43e20f413812a2fbea3383e29370c166ded
ishrar <ishrarhussain@gmail.com>
2014-07-30 16:29:29 -0400

    Translated Versions of Mornin's AKI Project Query
    These two are correctly translated versions of Mornin's AKI Project
    query

--- sql_codes/aki_definition_hana.sql
+++ sql_codes/aki_definition_hana.sql
@@ -1,116 +1,75 @@
-/******************************************************************************/
-/***************    Cohort Extraction for AKI project      ********************/
-/***************    Created by Mornin Apr 2014              ********************/
-/***************    Translating oracle sql codes to hana sql ********************/
-/******************************************************************************/
+-- Translated for SAP HANA by Ishrar
+-- Runtime Recorded on SAP HANA @Cambridge HF1 Server: 37.728 seconds
 
+WITH "population" AS
+	( SELECT "ICUSTAY_ID","SUBJECT_ID","ICUSTAY_INTIME","ICUSTAY_OUTTIME" FROM "MIMIC2V26"."icustay_detail"
+		WHERE "ICUSTAY_AGE_GROUP"='adult'
+			AND "SUBJECT_ICUSTAY_SEQ"=1
+			AND "WEIGHT_FIRST" IS NOT NULL
+			AND "WEIGHT_FIRST" > 0
+	)
 
-with population as
-(select icustay_id
-,SUBJECT_ID
-,ICUSTAY_INTIME
-,ICUSTAY_OUTTIME 
-from "MIMIC2V26"."icustay_detail"
-where ICUSTAY_AGE_GROUP='adult'
-and SUBJECT_ICUSTAY_SEQ=1 -- error corrected by mornin!
-and weight_first is not null
-and weight_first >0
---and ICUSTAY_OUTTIME >= ICUSTAY_INTIME+interval '24' hour ---1st 15 (control+buffer) +3 (case) +6(aki) 
---and icustay_id<1000
-)
+-- gather uo data--table_temp is just all pts with certain itemid
+, "uo_table" AS
+	(	SELECT
+			"io"."ICUSTAY_ID",
+			"io"."CHARTTIME" 
+			,MAX("VOLUME") "max_vol"
+		FROM "MIMIC2V26"."ioevents" "io", "population" "pop"
+		WHERE "io"."ICUSTAY_ID"="pop"."ICUSTAY_ID"
+		AND "io"."ITEMID" in(651, 715, 55, 56, 57, 61, 65, 69, 85, 94, 96, 288, 405,
+			428, 473, 2042, 2068, 2111, 2119, 2130, 1922, 2810, 2859,
+			3053, 3462, 3519, 3175, 2366, 2463, 2507, 2510, 2592,
+			2676, 3966, 3987, 4132, 4253, 5927 )
+		GROUP BY "io"."ICUSTAY_ID" , "CHARTTIME"
+	)
 
---select * from population;
---, temp as
---(
---select io.icustay_id 
---from population pop
---join "MIMIC2V26"."ioevents" io
---on io.icustay_id=pop.icustay_id
---)
+-- calculating time span between uo readings
+,"uo_time_span" AS
+	( SELECT 
+		"ICUSTAY_ID",
+		"CHARTTIME", 
+		"max_vol",
+		( CASE
+			WHEN ( "ICUSTAY_ID" = LAG( "ICUSTAY_ID" ) OVER( ORDER BY "ICUSTAY_ID" ) ) 
+			THEN EXTRACT( DAY FROM "CHARTTIME" ) - EXTRACT( DAY FROM ( LAG( "CHARTTIME" ) OVER( ORDER BY "ICUSTAY_ID" ) ) )*24
+			+ EXTRACT( HOUR FROM "CHARTTIME" ) - EXTRACT( HOUR FROM ( LAG( "CHARTTIME" ) OVER( ORDER BY "ICUSTAY_ID" ) ) )
+			+ EXTRACT( MINUTE FROM "CHARTTIME" ) - EXTRACT( MINUTE FROM ( LAG( "CHARTTIME" ) OVER( ORDER BY "ICUSTAY_ID" ) ) )/60
+		ELSE NULL
+		END) AS "TIME_SPAN"
+		FROM "uo_table"
+	)
 
---select * from temp;
+-- calculate the normalized uo rate
+,"normalized_uo" AS
+	( SELECT "uo"."ICUSTAY_ID" ,"CHARTTIME","max_vol","TIME_SPAN","icud"."WEIGHT_FIRST"
+		, ROUND( "uo"."max_vol" / "uo"."TIME_SPAN" / "icud"."WEIGHT_FIRST", 3 ) as "uo_rate"
+		FROM "uo_time_span" "uo" 
+		LEFT JOIN "MIMIC2V26"."icustay_detail" "icud" ON "uo"."ICUSTAY_ID" = "icud"."ICUSTAY_ID"
+		WHERE "uo"."TIME_SPAN" IS NOT NULL
+	)
 
+, "aki_onset_1" AS
+	( SELECT 
+		"a"."ICUSTAY_ID"
+		,"a"."CHARTTIME" AS "onset_time"
+		, ROUND( AVG("b"."uo_rate"), 2 ) AS "uo_mean"
+		FROM "normalized_uo" "a"
+		JOIN "normalized_uo" "b" ON "a"."ICUSTAY_ID" = "b"."ICUSTAY_ID"
+		WHERE 
+		"b"."CHARTTIME" BETWEEN "a"."CHARTTIME" AND ADD_SECONDS("a"."CHARTTIME", 60*60*6)
+		GROUP BY "a"."ICUSTAY_ID", "a"."CHARTTIME"
+		ORDER BY 1,2
+	)
 
---gather uo data--table_temp is just all pts with certain itemid
-, uo_table as
-(
-	select 
-		io.ICUSTAY_ID,
-		io.CHARTTIME 
-		,max(VOLUME) max_vol
-  from "MIMIC2V26"."ioevents" io, population pop
-	where io.ICUSTAY_ID=pop.ICUSTAY_ID
-	and io.ITEMID in(651, 715, 55, 56, 57, 61, 65, 69, 85, 94, 96, 288, 405,
-                       428, 473, 2042, 2068, 2111, 2119, 2130, 1922, 2810, 2859,
-                       3053, 3462, 3519, 3175, 2366, 2463, 2507, 2510, 2592,
-                       2676, 3966, 3987, 4132, 4253, 5927 )                    
-	group by io.ICUSTAY_ID , CHARTTIME 
-)
+, "aki_onset" as
+	(
+	SELECT DISTINCT 
+		"ICUSTAY_ID"
+		, "onset_time"
+		, "uo_mean"
+		, CASE WHEN "uo_mean"< 0.5 THEN 1 ELSE 0 END AS "aki_flg"
+		FROM "aki_onset_1"
+	)
 
---select * from uo_table;
-select (charttime)-1 as temp from uo_table;
-
---------------- calculating time span between uo readings ----------------
-,uo_time_span as
-(
-	select 
-		ICUSTAY_ID,
-		CHARTTIME, 
-    max_vol,    
-   (case
-				when (icustay_id=lag(icustay_id) over(order by icustay_id)) 
-				then EXTRACT(day FROM (CHARTTIME-(lag(charttime) over(order by icustay_id))))*24 
-        + EXTRACT(hour FROM (CHARTTIME-(lag(charttime) over(order by icustay_id))))
-        +extract(minute from (CHARTTIME-(lag(charttime) over(order by icustay_id))))/60
-			else NULL
-		end) as time_span
-	from uo_table
-)
-select * from uo_time_span;
-
--------------------- calculate the normalized uo rate -------------------------
-,normalized_uo as
-(
-	select uo.ICUSTAY_ID ,CHARTTIME,max_vol,time_span,icud.weight_first
-  ,round(uo.max_vol/uo.time_span/icud.WEIGHT_first,3) as uo_rate
-	from uo_time_span uo 
-  left join mimic2v26.ICUSTAY_DETAIL icud on uo.ICUSTAY_ID=icud.ICUSTAY_ID 
-	--left join population C on a.ICUSTAY_ID=c.ICUSTAY_ID 
-	where uo.time_span is not null
-)
-
-
-, aki_onset_1 as
-(select 
-a.icustay_id
-,a.charttime as onset_time
---, a.uo_rate as onset_uo
---, b.charttime
---, b.uo_rate
-, round(avg(b.uo_rate),2) as uo_mean
-from normalized_uo a --what to do here.
-join normalized_uo b on a.icustay_id=b.icustay_id
-where 
---a.uo_rate<0.5
---and 
-b.charttime between a.charttime and a.charttime+6/24 --do i have to change this?
---and b.charttime>a.charttime
-group by a.icustay_id, a.charttime
-order by 1,2
-)
-
---select * from aki_onset_1; --606sec
-
-
-
-, aki_onset as
-(
-select distinct 
-icustay_id
-, onset_time
-, uo_mean
-, case when uo_mean<0.5 then 1 else 0end as aki_flg
-from aki_onset_1
-)
-
-select * from aki_onset;
+SELECT * FROM "aki_onset";
