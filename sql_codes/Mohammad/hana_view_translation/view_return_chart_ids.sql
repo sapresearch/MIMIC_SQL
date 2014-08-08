972790eb3e8dda4a97ab0c127a79526c1ced641f
ishrar <ishrarhussain@gmail.com>
2014-08-08 16:44:10 -0400

    Translation of Mohammad's Queries into HANA's Views
    An example procedure for building dynamic views in Hana, using
    Mohammad's queries.

---
+++ sql_codes/Mohammad/hana_view_translation/view_return_chart_ids.sql
@@ -0,0 +1,24 @@
+DROP PROCEDURE view_return_chart_ids;
+CREATE PROCEDURE view_return_chart_ids (IN q_chart_name VARCHAR)
+	LANGUAGE SQLSCRIPT AS
+
+BEGIN
+	DECLARE query_string STRING;
+	DECLARE view_exists INT := 0;
+	SELECT COUNT(*) INTO view_exists FROM "VIEWS" WHERE "SCHEMA_NAME" = 'MIMIC2V26' AND "VIEW_NAME" = 'RETURN_CHART_IDS';
+	IF :view_exists = 1 THEN
+		EXEC 'DROP VIEW "MIMIC2V26"."RETURN_CHART_IDS"';
+	END IF;
+	query_string := 'CREATE VIEW "MIMIC2V26"."RETURN_CHART_IDS" AS (
+			SELECT	"ev"."ITEMID" AS "ITEMID",
+				"di"."LABEL" AS "LABEL",
+				COUNT("ev"."ITEMID") AS "COUNT_ITEMID"
+			FROM "MIMIC2V26"."chartevents"	"ev"
+				JOIN "MIMIC2V26"."d_chartitems"	"di"
+			ON "di"."ITEMID" = "ev"."ITEMID"
+			WHERE LOWER("di"."LABEL") LIKE ''%'|| :q_chart_name || '%''
+			GROUP BY "ev"."ITEMID", "di"."LABEL"
+			ORDER BY "COUNT_ITEMID" DESC
+		)';
+	EXEC query_string;
+END;
