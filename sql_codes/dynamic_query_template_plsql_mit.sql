c9bf3c6ed74af59988069171ae5a747d04f613f8
mornin <mornin@gmail.com>
2014-09-04 17:22:56 -0400

    Comments added

--- sql_codes/dynamic_query_template_plsql_mit.sql
+++ sql_codes/dynamic_query_template_plsql_mit.sql
@@ -1,3 +1,8 @@
+---------- Dynamic/Functional SQL Template for PL/SQL ------------
+-------- Created by Mornin Feng on 07/12/14 ----------------------
+
+-- Creating column definitions for the output table
+
 create or replace force type column_t as object 
 (SUBJECT_ID	NUMBER(7,0),
 HADM_ID	NUMBER(7,0),
@@ -6,9 +11,10 @@ CODE	VARCHAR2(100 BYTE),
 DESCRIPTION	VARCHAR2(255 BYTE)
 );
 
+-- Creating table type for the output table
 create or replace type icd9set_t as table of column_t;
 
-
+-- Defining the dynamic sql function
 create or replace function return_table(q_subject_id in ICD9.SUBJECT_ID%type)
 return icd9set_t as
   v_ret   icd9set_t;
@@ -16,6 +22,7 @@ begin
   select 
   cast(
   multiset(
+    -- The SQL code goes here!!!
     select * from icd9 where subject_id=q_subject_id)
     as icd9set_t)
     into
@@ -27,4 +34,5 @@ begin
   
 end return_table;
 
+-- Testing the defined function
 select * from table(return_table(44));
